0b6611147da07f0d5b5cde6fa8e9ca8fa4c61dd2
Yicvot <2653609691@qq.com>
2023-10-13 15:33:15 +0800

    稍微整理了一下

---
+++ Api_and_Library_Learn/OpenGL_Imgui/src/render.h
@@ -0,0 +1,63 @@
+//
+// Created by lenovo on 9/28/2023.
+//
+
+#ifndef LEARN_RECORD_RENDER_H
+#define LEARN_RECORD_RENDER_H
+
+#include "glad/glad.h"
+#include "shader.h"
+//#include "camera.h"
+#include "window.h"
+#include "camera.h"
+
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb-master/stb_image.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "unistd.h"
+
+
+
+namespace yic{
+
+
+    class Render{
+    public:
+
+        Render(int w, int h, Window window);
+        ~Render();
+
+        Render(const Render&) = delete;
+        Render &operator = (const Render&) = delete;
+
+
+
+        void setBuffer();
+        //static void loadTexture();
+        void render(float w, float h);
+        void loadTexture();
+
+
+
+
+    private:
+        Window window;
+
+        unsigned int VBO, VAO, EBO{};
+        float width{}, height{};
+        unsigned int texture1, texture2;
+        glm::mat4 view{};
+
+        char buf[256];
+
+        Shader shader{R"(F:\Program\LearningRecord\Api_and_Library_Learn\OpenGL_Imgui\src\shader\shader.vert)", R"(F:\Program\LearningRecord\Api_and_Library_Learn\OpenGL_Imgui\src\shader\shader.frag)"};
+        //Shader shader{"../src/shader/shader.vert", "../src/shader/shader.frag"};
+    };
+}
+
+#endif //LEARN_RECORD_RENDER_H
---
+++ Api_and_Library_Learn/Vulkan_C++_Style/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by lenovo on 10/13/2023.
+//
+//#include "vulkan/vulkan.h"
+#include "iostream"
+
+int main(){
+
+    std::cout << 3 << std::endl;
+
+    return 0;
+}
